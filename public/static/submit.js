caa83f1c838737030b6be01b1178a33b03662a3a
JakesTechSpace <techlead@jakestechspace.com>
2021-03-19 20:16:20 -0400

    update url

--- public/static/submit.js
+++ public/static/submit.js
@@ -12,7 +12,7 @@
 
     e.preventDefault()
 
-    fetch("http://localhost:3000/sentiment", {
+    fetch("https://text-analysis-backend.herokuapp.com/sentiment", {
       method: "POST",
       body: JSON.stringify({ text: formInput.value }),
       headers: myHeaders
