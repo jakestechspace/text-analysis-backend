7428e09253dea72eb52f42415224dbac372065bb
JakesTechSpace <techlead@jakestechspace.com>
2021-03-20 15:43:30 -0400

    update comment

--- rateLimit.js
+++ rateLimit.js
@@ -1,7 +1,8 @@
 const rateLimit = require("express-rate-limit");
 
+// limit api to 10 requests / min
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 2 min
+  windowMs: 1 * 60 * 1000,
   max: 10
 });
 
